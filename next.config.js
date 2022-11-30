5c376d176f7a1537dca96f68362614c27133c670
고지수 <gosjioo0312@naver.com>
2022-11-30 22:53:16 +0900

    ddd

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  assetPrefix:
-    process.env.NODE_ENV === 'production'
-    ? 'https://jisookoh.github.io/'
-    : '',
 }
 
 module.exports = nextConfig
