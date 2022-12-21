84fb81db1eede3febe55297f35bbbe4551e37a4a
고지수 <gosjioo0312@naver.com>
2022-12-21 20:54:43 +0900

    ADD: 영화 API baseURL 수정

--- store/modules/movie/apiDocs.ts
+++ store/modules/movie/apiDocs.ts
@@ -1,5 +1,5 @@
 
-const baseUrl = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json";
+const baseUrl = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json";
 const apiKey = process.env.NEXT_PUBLIC_MOVIE_API_KEY;
 
 export const apiDocs = {
